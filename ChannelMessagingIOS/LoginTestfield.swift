8ff6dcca3c5357a9a2aa2c43ff688179230c201d
Eddy Ekofo <eddyecko@gmail.com>
2017-03-19 16:37:03 +0100

    made two input fields

--- ChannelMessagingIOS/LoginTestfield.swift
+++ ChannelMessagingIOS/LoginTestfield.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
+@IBDesignable
 class LoginTestfield: UITextField {
 
- 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    
+        self.layer.borderColor = UIColor(white: 231/255, alpha: 1).cgColor
+        self.layer.borderWidth = 1
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 8 , dy: 7 )
+    }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds: bounds)
+    }
 
 }
