77b9852fbbc4feb2b81877d176d6b135fc63784c
Eddy Ekofo <eddyecko@gmail.com>
2017-03-19 15:27:39 +0100

    Added a subclass for the textview

---
+++ ChannelMessagingIOS/LoginTestfield.swift
@@ -0,0 +1,15 @@
+//
+//  LoginTestfield.swift
+//  ChannelMessagingIOS
+//
+//  Created by Eddy Ekofo on 19/03/2017.
+//  Copyright © 2017 Eddy Ekofo. All rights reserved.
+//
+
+import UIKit
+
+class LoginTestfield: UITextField {
+
+ 
+
+}
